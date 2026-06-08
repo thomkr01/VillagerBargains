357dee6cee4dea1fe0fa484cfa604b5012c3e6a3
Tommy <github@thomkr.com>
2026-06-08 13:02:12 +0200

    chore: remove old com.bargainvillage trade classes

--- src/main/java/com/bargainvillage/trade/TradeDefinition.java
+++
@@ -1,7 +0,0 @@
-package com.bargainvillage.trade;
-
-public record TradeDefinition(String tradeId, int vanillaMin, int vanillaMax) {
-    public int clamp(int requested) {
-        return Math.max(vanillaMin, Math.min(vanillaMax, requested));
-    }
-}
